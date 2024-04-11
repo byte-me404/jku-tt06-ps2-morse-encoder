ae9095950983ee0727e841428e5ebf28a298e1ec
Daniel <db.baumgartner@gmx.at>
2024-04-11 12:16:33 +0200

    update

--- src/config.tcl
+++ src/config.tcl
@@ -16,7 +16,7 @@ set ::env(PL_TARGET_DENSITY) 0.85
 
 # CLOCK_PERIOD - Increase this in case you are getting setup time violations.
 # The value is in nanoseconds, so 20ns == 50MHz.
-set ::env(CLOCK_PERIOD) "500"
+set ::env(CLOCK_PERIOD) "1000"
 
 # Hold slack margin - Increase them in case you are getting hold violations.
 set ::env(PL_RESIZER_HOLD_SLACK_MARGIN) 0.1
